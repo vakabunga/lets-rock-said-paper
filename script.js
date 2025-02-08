4e1618deea1c14eb18793ea90c019b06fc1eac55
svetaslpnv <k1nderarzt@yandex.ru>
2025-02-08 20:21:19 +0300

    fixed_script.js
    добавила в глобальную переменную экран игры, исправила url в fetch

--- script.js
+++ script.js
@@ -1,71 +1,91 @@
 const app = document.querySelector('.app');
 
-function gameChooseYourMoveTemplate() {
-    return {
-        block: 'div',
-        cls: 'game-wrapper',
-        content: [{
+window.application = {
+    player: {
+        login: undefined,
+        token: undefined,
+    },
+    screensTemplates: {
+        welcomeScreenTemplate: () => {return {}},
+        lobbyScreenTemplate: () => {return {}},
+        gameScreenTemplate: () => {return {
             block: 'div',
-            cls: 'game__header',
+            cls: 'game-wrapper',
             content: [{
-                block: 'h1',
-                cls: 'game__title',
-                content: 'Игра',
-            }, {
                 block: 'div',
-                cls: 'game__subtitle',
-                content: 'Вы против *имясоперника*',
+                cls: 'game__header',
+                content: [{
+                    block: 'h1',
+                    cls: 'game__title',
+                    content: 'Игра',
+                }, {
+                    block: 'div',
+                    cls: 'game__subtitle',
+                    content: 'Вы против *имясоперника*',
+                }],
+                block: 'div',
+                cls: 'game__choose-move',
+                content: [{
+                    block: 'button',
+                    cls: 'rock',
+                    content: 'Камень',
+                    attrs: {
+                        'data-move': 'rock',
+                    }
+                }, {
+                    block: 'button',
+                    cls: 'paper',
+                    content: 'Бумага',
+                    attrs: {
+                        'data-move': 'paper',
+                    }
+                }, {
+                    block: 'button',
+                    cls: 'scissors',
+                    content: 'Ножницы',
+                    attrs: {
+                        'data-move': 'scissors',
+                    }
+                }],
             }],
-            block: 'div',
-            cls: 'game__choose-move',
-            content: [{
-                block: 'button',
-                cls: 'rock',
-                content: 'Камень',
-                attrs: {
-                    'data-move': 'rock',
-                }
-            }, {
-                block: 'button',
-                cls: 'paper',
-                content: 'Бумага',
-                attrs: {
-                    'data-move': 'paper',
-                }
-            }, {
-                block: 'button',
-                cls: 'scissors',
-                content: 'Ножницы',
-                attrs: {
-                    'data-move': 'scissors',
-                }
-            }],
-        }],
-    };
-}
+        }},
+        spinnerScreenTemplate: () => {return {}},
+        resultScreenTemplate: () => {return {}},
+    },
+    renderScreen: function (template) {
+        app.innerHTML = browserTemplateEngine(template()).innerHTML;
+    },
+    timers: [],
+};
+
+window.application.renderScreen(gameScreenTemplate());
     
-app.innerHTML = browserTemplateEngine(gameChooseYourMoveTemplate()).innerHTML;
-    
-function play () {
+function play() {
     const token = window.application.player.token;
     const id = window.application['player-status'].game.id;
 
-    fetch(`http://localhost:3000/play?token=${token}&id=${id}&move=${move}`)
+    const gameUrl = 'https://skypro-rock-scissors-paper-backend.vercel.app/';
+
+    fetch(`${gameUrl}/play?token=${token}&id=${id}&move=${move}`)
         .then((response) => {response.json()})
         .then(data => {
             const gameStatus = data['game-status'].status;
 
             if (gameStatus === "waiting-for-your-move") {
+                window.application.renderScreen(gameScreenTemplate());
                 //отрисовка экрана ничья, ожидаем ваш ход
             }
             if (gameStatus === "waiting-for-enemy-move") {
+                window.application.renderScreen(spinnerScreenTemplate());
                 //отрисовка экрана ждем экрана соперника
             }
             if (gameStatus === "lose") {
+                window.application.renderScreen(resultScreenTemplate());
                 //отрисовка экрана вы проиграли
             }
             if (gameStatus === "win") {
-                //отрисовка экрана вы проиграли
+                window.application.renderScreen(resultScreenTemplate());
+                //отрисовка экрана вы выиграли
             }
             if (gameStatus = 'error') {
                 console.log(data.message)
@@ -83,6 +103,6 @@ app.addEventListener('click', function (event) {
     if (event.target.hasAttribute('data-move')) {
         const move = event.target.className;
 
-        play (move);
+        play(move);
     }
 })
